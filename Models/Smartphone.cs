bb07820a79a5c4ca7ccb58b156f730d2fe70a461
Thainá Letícia Dias da Costa <thainadiasc@gmail.com>
2026-03-06 07:35:46 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,18 @@
+using System;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
         }
 
         public void Ligar()
